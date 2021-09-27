b90df54ac05e3c8c03012b853b9631bcdc5a8cf7
Greg Teixeira <greg.teixeira123@gmail.com>
2021-09-27 02:09:46 -0300

    refactor: create util func to get cqrs handlers

---
+++ src/common/utils/cqrs.util.ts
@@ -0,0 +1,22 @@
+import { Provider } from '@nestjs/common'
+import { isFunction } from './function.util'
+
+/**
+ * @param exportedValues pass the exports
+ * @param searchStr @default 'handler'
+ * @example
+ * ```ts
+ * export * from './command1'
+ * export * from './command2'
+ *
+ * export const CommandHandlers = getCqrsHandlers(exports)
+ * ```
+ */
+export const getCqrsHandlers = (
+  exportedValues: any,
+  searchStr: string = 'handler'
+): Provider[] => {
+  return Object.values(exportedValues).filter(
+    handler => isFunction(handler) && new RegExp(searchStr, 'i').test(handler.name)
+  ) as Provider[]
+}
--- src/modules/users/cqrs/commands/index.ts
+++ src/modules/users/cqrs/commands/index.ts
@@ -1,9 +1,6 @@
-import { Provider } from '@nestjs/common'
-import { isFunction } from '@common/utils'
+import { getCqrsHandlers } from '@common/utils'
 
 export * from './sign-up'
 export * from './login'
 
-export const CommandHandlers = Object.values(exports).filter(
-  handler => isFunction(handler) && /handler/i.test(handler.name)
-) as Provider[]
+export const CommandHandlers = getCqrsHandlers(exports)
