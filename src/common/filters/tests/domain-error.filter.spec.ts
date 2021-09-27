81150349f161ce8f646b55055e72f75460a47489
Greg Teixeira <greg.teixeira123@gmail.com>
2021-09-27 03:33:29 -0300

    test: domain error filter

---
+++ src/common/filters/tests/domain-error.filter.spec.ts
@@ -0,0 +1,17 @@
+import { ArgumentsHost } from '@nestjs/common'
+import { createMock } from '@golevelup/nestjs-testing'
+import { UnauthenticatedError } from '@common/errors'
+import { DomainErrorFilter } from '../domain-error.filter'
+
+describe('DomainErrorFilter', () => {
+  const domainErrorFilter = new DomainErrorFilter()
+
+  it('should return domain error', () => {
+    const host = createMock<ArgumentsHost>()
+    host.getType.mockReturnValue('graphql')
+    host.getArgs.mockReturnValue([])
+    const error = domainErrorFilter.catch(new UnauthenticatedError(), host)
+
+    expect(error).toBeInstanceOf(UnauthenticatedError)
+  })
+})
