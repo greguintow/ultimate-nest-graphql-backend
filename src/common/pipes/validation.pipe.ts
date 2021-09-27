18ac01a2c7f780a46b3ea4c87cdfdc6cb25ba290
Greg Teixeira <greg.teixeira123@gmail.com>
2021-09-27 04:58:09 -0300

    refactor: validation pipe to formatErrors be a method

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -1,24 +1,24 @@
-import { ValidationPipe as NestValidationPipe } from '@nestjs/common'
+import { ValidationError, ValidationPipe as NestValidationPipe } from '@nestjs/common'
 import { InvalidArgumentsError } from '@common/errors'
 
 export class ValidationPipe extends NestValidationPipe {
   constructor() {
     super({
       disableErrorMessages: false,
-      exceptionFactory: errors => {
-        return new InvalidArgumentsError({
-          fields: errors.map(({ property, constraints }) => ({
-            name: property,
-            message: !constraints ? [] : Object.values(constraints),
-            constraints: !constraints
-              ? []
-              : Object.keys(constraints).map(type => ({
-                  type,
-                  message: constraints[type]
-                }))
-          }))
-        })
-      }
+      exceptionFactory: ValidationPipe.formatErrors
+    })
+  }
+
+  static formatErrors(errors: ValidationError[]) {
+    return new InvalidArgumentsError({
+      fields: errors.map(({ property, constraints = {} }) => ({
+        name: property,
+        message: Object.values(constraints),
+        constraints: Object.keys(constraints).map(type => ({
+          type,
+          message: constraints[type]
+        }))
+      }))
     })
   }
 }
