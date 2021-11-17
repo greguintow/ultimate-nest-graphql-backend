10de2bb3a9278ee34d6ad7d3a63a3be914b68018
Greg Teixeira <greg.teixeira123@gmail.com>
2021-11-17 17:29:24 -0300

    feat: add new methods to base repository class

--- src/common/classes/base-repository-prisma.class.ts
+++ src/common/classes/base-repository-prisma.class.ts
@@ -24,11 +24,46 @@ export abstract class BaseRepositoryPrisma<
     return count > 0
   }
 
+  async getById(id: string): Promise<T | null> {
+    // @ts-ignore
+    const result = await this.prismaService[this.table].findUnique({
+      where: {
+        id
+      }
+    })
+    return this.format(result)
+  }
+
+  async getByIds(ids: string[]): Promise<T[]> {
+    // @ts-ignore
+    const result = await this.prismaService[this.table].findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    })
+    return this.formatMany(result)
+  }
+
+  async existsById(id: string): Promise<boolean> {
+    const exists = await this.exists({ id })
+    return exists
+  }
+
   protected format<V extends AllPrismaModels[U] | null>(obj: V): V extends null ? null : T {
     if (!obj) {
       return null as V extends null ? null : T
     }
 
     return plainToClass(this.Model, obj) as V extends null ? null : T
+  }
+
+  protected formatMany<V extends AllPrismaModels[U]>(obj: V[]): T[] {
+    if (!obj) {
+      return [] as T[]
+    }
+
+    return obj.map(this.format) as T[]
   }
 }
