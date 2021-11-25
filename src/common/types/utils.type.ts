57471d33a00759c83430386ce6f960ad5b88db31
Greg Teixeira <thunderdance6@gmail.com>
2021-11-25 01:35:59 -0300

    feat: add GetWhereUniqueInput utility type

--- src/common/types/utils.type.ts
+++ src/common/types/utils.type.ts
@@ -35,6 +35,10 @@ export type GetWhereInput<T extends PrismaTables> = NonNullable<
   NonNullable<UnpackSubset<Parameters<PrismaClient[T]['count']>[0]>>['where']
 >
 
+export type GetWhereUniqueInput<T extends PrismaTables> = NonNullable<
+  NonNullable<UnpackSubset<Parameters<PrismaClient[T]['findUnique']>[0]>>['where']
+>
+
 export type AllPrismaModels = {
   [K in PrismaTables]: GetModelType<K>
 }
@@ -43,4 +47,15 @@ export type AllPrismaWhereInput = {
   [K in PrismaTables]: GetWhereInput<K>
 }
 
+export type AllPrismaWhereUniqueInput = {
+  [K in PrismaTables]: GetWhereUniqueInput<K>
+}
+
 export type $Values<T extends Obj> = T[keyof T]
+
+export type PickByValue<T, ValueType> = Pick<
+  T,
+  { [Key in keyof T]-?: T[Key] extends ValueType ? Key : never }[keyof T]
+>
+
+export type PrimitiveKeys<T> = keyof PickByValue<T, string>
