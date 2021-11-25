086886b23811cfeb0b5a2313d6c45390eafd4475
Greg Teixeira <thunderdance6@gmail.com>
2021-11-25 01:17:30 -0300

    feat: add getAllErrors method in validation pipe

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -1,5 +1,12 @@
-import { ValidationError, ValidationPipe as NestValidationPipe } from '@nestjs/common'
+import { ValidationPipe as NestValidationPipe } from '@nestjs/common'
+import { ValidationError } from 'class-validator'
 import { InvalidArgumentsError } from '@common/errors'
+
+interface ValidationErrorConstraints extends ValidationError {
+  constraints: {
+    [type: string]: string
+  }
+}
 
 export class ValidationPipe extends NestValidationPipe {
   constructor() {
@@ -10,8 +17,10 @@ export class ValidationPipe extends NestValidationPipe {
   }
 
   static formatErrors(errors: ValidationError[]) {
+    const allErrors = ValidationPipe.getAllErrors(errors)
+
     return new InvalidArgumentsError({
-      fields: errors.map(({ property, constraints = {} }) => ({
+      fields: allErrors.map(({ property, constraints }) => ({
         name: property,
         message: Object.values(constraints),
         constraints: Object.keys(constraints).map(type => ({
@@ -20,5 +29,20 @@ export class ValidationPipe extends NestValidationPipe {
         }))
       }))
     })
+  }
+
+  static getAllErrors(errors: ValidationError[], parentKey?: string): ValidationErrorConstraints[] {
+    return errors
+      .reduce((arr, error) => {
+        const newKey = parentKey ? `${parentKey}.${error.property}` : error.property
+        error.property = newKey
+
+        if (error.children && error.children.length > 0) {
+          return [...arr, error, ...ValidationPipe.getAllErrors(error.children, error.property)]
+        }
+
+        return [...arr, error]
+      }, [] as ValidationError[])
+      .filter(error => error.constraints) as ValidationErrorConstraints[]
   }
 }
