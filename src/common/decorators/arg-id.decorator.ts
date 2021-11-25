b7046dc5c33fca3a33ecbe111c3d81d1146311d5
Greg Teixeira <thunderdance6@gmail.com>
2021-11-24 23:55:23 -0300

    chore: fix typo

--- src/common/decorators/arg-id.decorator.ts
+++ src/common/decorators/arg-id.decorator.ts
@@ -1,10 +1,10 @@
 import { Args, ArgsOptions, ID } from '@nestjs/graphql'
 
-export interface ArgsIdOptions extends ArgsOptions {
+export interface ArgIdOptions extends ArgsOptions {
   isArray?: boolean
 }
 
-export function ArgId(argName: string, options?: ArgsIdOptions): ParameterDecorator {
+export function ArgId(argName: string, options?: ArgIdOptions): ParameterDecorator {
   return (target, key, index) => {
     Args(argName, { ...options, type: () => (options?.isArray ? [ID] : ID) })(target, key, index)
   }
